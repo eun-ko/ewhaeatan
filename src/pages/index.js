1099f3d0a576024d8c2f5761e963224f045037d5
EunKo <koeun0529@gmail.com>
2021-01-08 20:10:14 +0900

    [feat] 페이지 라우팅 추가

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Intro from "./intro";
+import Location from "./location";
+import FoodType from "./foodType";
+import Result from "./result";
+import List from "./list";
+
+export {Intro,Location,FoodType,Result,List}
